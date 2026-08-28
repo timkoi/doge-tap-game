8cf3555e4476bc1fbff8eb2efd887befbc1370a6
timkoi <givemesometea9@gmail.com>
2026-08-29 01:44:54 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const { Telegraf } = require('telegraf');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const BOT_TOKEN = '8581159804:AAHzqC9moFkFuSWhWwBz7p2MdANOntZMv3A';
+const bot = new Telegraf(BOT_TOKEN);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+bot.start((ctx) => {
+    ctx.reply('Добро пожаловать в Doge Tap! Жми кнопку:', {
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: 'ИГРАТЬ', web_app: { url: 'https://ТВОЙ-ДОМЕН.com' } }]
+            ]
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log('Сервер запущен на порту ' + PORT);
+    bot.launch();
+    console.log('Бот запущен!');
+});
